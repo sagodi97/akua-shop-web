426e3aa6cecfef5f6586c933b92fcb61a93a6d55
Santiago Gonzalez <santiago.gonzalez@clarite.pl>
2020-09-11 11:59:46 +0200

    Add inital footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Santiago Gonzalez, all rights reserved</p>
+    </footer>
+  );
+}
+
+export default Footer;
